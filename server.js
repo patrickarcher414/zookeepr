6e1692ff962a73a1664640142a456d55609264dc
patrickarcher414 <patrickarcher414@gmail.com>
2022-05-04 22:47:11 -0500

    get param

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { animals } = require("./data/animals");
 const express = require("express");
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -45,6 +46,11 @@ function filterByQuery(query, animalsArray) {
   return filteredResults;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
+
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -53,6 +59,15 @@ app.get("/api/animals", (req, res) => {
   res.json(results);
 });
 
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
+
 app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+  console.log(`API server now on port ${PORT}!`);
 });
